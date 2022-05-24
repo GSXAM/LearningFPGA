931ffcaeeffce18fe31ea704da6c1afde8fce44e
GSXAM <luongcongduchandsome@gmail.com>
2022-05-24 21:38:10 +0700

    Add completed files

--- Srcs/clock_divider_50Hz.vhd
+++ Srcs/clock_divider_50Hz.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 04/24/2022 10:53:58 AM
+-- Create Date: 05/24/2022 01:59:31 PM
 -- Design Name: 
--- Module Name: Clock_divider_1Hz - Behavioral
+-- Module Name: clock_divider_50Hz - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,7 +21,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -31,15 +30,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity clock_divider_100MHz_to_1Hz is
-    Port ( clk_100Mhz : in  STD_LOGIC;	-- 100 MHz in clock
-           reset : in  STD_LOGIC;
-           clk_1Hz : out  STD_LOGIC);	-- 1Hz out clock
-end clock_divider_100MHz_to_1Hz;
+entity clock_divider_50Hz is
+    Port ( clk_100Mhz : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           clk_50Hz : out STD_LOGIC);
+end clock_divider_50Hz;
 
-architecture clock_RTL of clock_divider_100MHz_to_1Hz is
+architecture clock_RTL of clock_divider_50Hz is
 	signal temporal: STD_LOGIC;
-	signal counter : integer range 0 to 49999999 := 0;
+	signal counter : integer range 0 to 999999 := 0;
 begin
 	frequency_divider: process (reset, clk_100Mhz) 
 	begin
@@ -47,7 +46,7 @@ begin
             temporal <= '0';
             counter <= 0;
         elsif rising_edge(clk_100Mhz) then
-            if (counter = 49999999) then
+            if (counter = 999999) then
                 temporal <= NOT(temporal);
                 counter <= 0;
             else
@@ -55,5 +54,5 @@ begin
             end if;
         end if;
     end process;
-    clk_1Hz <= temporal;
+    clk_50Hz <= temporal;
 end clock_RTL;
